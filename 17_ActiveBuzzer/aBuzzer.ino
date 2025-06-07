1d7ba00775aa27a1f1f9dc6cca14958fc2fa5fa5
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2025-06-07 09:24:02 +0530

    Update and rename _17_ActiveBuzzer.ino to 17_ActiveBuzzer/aBuzzer.ino

---
+++ 17_ActiveBuzzer/aBuzzer.ino
@@ -0,0 +1,21 @@
+/*********************************************************** 
+File name:   aBuzzer.ino 
+Description:  Active Buzzer. 
+***********************************************************/ 
+
+int buzzerPin = 9; 
+ 
+void setup () 
+{   
+  pinMode (buzzerPin, OUTPUT); 
+} 
+ 
+void loop ()
+{   
+digitalWrite (buzzerPin, HIGH);  
+delay (500);   
+digitalWrite (buzzerPin, LOW);   
+delay (500); 
+} 
+ 
+ 
